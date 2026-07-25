337d1484b6270ff1141d59189571f5558f6f4cc4
zeroscript98-byte <zeroscript98@gmail.com>
2026-07-25 07:23:30 +0700

    Refactor memory update and command execution logic

--- ai.py
+++ ai.py
@@ -3,6 +3,7 @@ import re
 import subprocess
 import json
 import os
+import datetime
 
 API_KEY = ""
 URL = "https://api.groq.com/openai/v1/chat/completions"
@@ -26,11 +27,11 @@ important_memory = [
       -dont make or execute a dangerous bash
       -dont execute a bash that could make error this device
       bash:
-      -dont create a <cmd></cmd> more than one if the bash do something more than one just put it together
+      -dont create a <cmd></cmd> more than one if the bash have to do something more than one just put it together
       -if we were talking or user ask you to execute a bash,put the cmd at the top of your message 
        like in a differrent line and create a empty line to separate it with your message
       -if we werent talk about bash then dont message or dont show the cmd tags in your message even at the first chat
-      -because if the cmd tag appear in your message the bash will automatically executing so be careful
+      -because if the <cmd></cmd> tag appear in your message the bash will automatically executing so be careful
       
       '''
   },
@@ -65,71 +66,35 @@ def update_history(ai,hs,user):
   with open("history.json","w") as f:
     json.dump(hs,f,indent=2)
     
-def update_memory(hs):
+def update_memory(hs,mf,user):
   memory_prompt = """
-  You are a memory manager.
-    Your only job is to determine whether the latest conversation contains information worth remembering for future conversations.
-    Rules:
-    - Only save long-term information.
-    - Ignore temporary information.
-    - Ignore greetings.
-    - Ignore random questions.
-    - Ignore assistant responses unless they contain user preferences.
-
-    Long-term information includes:
-    - Users name
-    - Nickname
-    - Preferences
-    - Personality preference
-    - Ongoing projects
-    - Programming languages they often use
-    - Frequently used tools
-    - Long-term goals
-    
-    Temporary information includes:
-    - Current weather
-    - One-time questions
-    - One-time commands
-    - Current directory
-    - Temporary errors
-    -anything else like those
-    Output ONLY valid JSON.
-    
-    Format:
-    {
-      "add": [
-        {
-          "key": "...",
-          "value": "..."
-        }
-      ],
-      "update": [
-        {
-          "key": "...",
-          "value": "..."
-        }
-      ],
-      "delete": [
-        "..."
-      ]
-    }
-    
-    If nothing should be remembered, return:
-    
-    {
-      "add": [],
-      "update": [],
-      "delete": []
-    }
+    Return ONLY valid JSON.
+  Always return a JSON array.
+  If nothing should be remembered:
+  []
+  If something should be remembered:
+  [
+      {
+          "action":"add",
+          "category":"...",
+          "key":"...",
+          "value":"..."
+      }
+  ]
+      
+      YOU MUST KNOW WHICH ONE YOU SHOULD REMEMBER AND YOU SHOULDNT
   """
   messages_memory = [
     {
         "role": "system",
         "content": memory_prompt
+    },
+    {
+        "role": "user",
+        "content": user
     }
     ]
-  if len(hs) > 20:
-    ai_memory = requests.post(
+  ai_memory = requests.post(
           URL,
           headers={
               "Authorization": f"Bearer {API_KEY}",
@@ -140,8 +105,39 @@ def update_memory(hs):
               "messages": messages_memory
           }
       )
-    summary = ai_memory.json()["choices"][0]["message"]["content"]
-
+  changes = json.loads(
+   ai_memory.json()["choices"][0]["message"]["content"])
+  for change in changes:
+        action = change["action"]
+        key = change["key"]
+        if action == "add":
+            if not any(item["key"] == key for item in mf):
+                mf.append(change)
+        elif action == "update":
+            for item in mf:
+                if item["key"] == key:
+                    item.update(change)
+                    break
+        elif action == "delete":
+            memory = [item for item in mf if item["key"] != key]
+  return mf
+  
+def bash_cmd():
+    bash = re.search(r"<cmd>(.*?)</cmd>", answer, re.DOTALL)
+    if bash:
+     command = bash.group(1).strip()
+     while True:
+       inp=input(f'"{command}" Wanna execute this command(y/n)? '.lower())
+       if inp == 'y':
+         subprocess.run(command,shell=True,
+                text=True,
+                capture_output=True
+            )
+       elif inp == 'n':
+         break
+       else:
+         print('Invalid input!')
+  
 def load_memory():
     if not os.path.exists("memory.json"):
         return []
@@ -190,13 +186,9 @@ while True:
     )
     answer = response.json()["choices"][0]["message"]["content"]
     update_history(answer,history_file,message)
-    update_memory(history_file)
+    memory=update_memory(history_file,memory_file,message)
+    with open("memory.json", "w") as f:
+     json.dump(memory, f, indent=2)
     print("AI:", answer)
-    bash = re.search(r"<cmd>(.*?)</cmd>", answer, re.DOTALL)
-    if bash:
-     command = bash.group(1).strip()
-     subprocess.run(command,shell=True,
-            text=True,
-            capture_output=True
-        )
+    bash_cmd()
   
